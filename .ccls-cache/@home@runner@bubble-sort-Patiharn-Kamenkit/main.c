ed521c805f90e1622bc21a654c68773a9a797e86
Patiharn-Kamenk <122970698+Patiharn-Kamenkit@users.noreply.github.com>
2023-01-27 10:32:47 +0000

    bubble beep beep

--- .ccls-cache/@home@runner@bubble-sort-Patiharn-Kamenkit/main.c
+++ .ccls-cache/@home@runner@bubble-sort-Patiharn-Kamenkit/main.c
@@ -1,42 +1,40 @@
+#include "sorting.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "sorting.h"
 
-int Isprime(int n)
-{
-  if(n==1) return 0;
-  for(int i=2; i<n; i++)
-    {
-      if(n % i ==0) return 0;
-    }
+int Isprime(int n) {
+  if (n == 1)
+    return 0;
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0)
+      return 0;
+  }
   return 1;
 }
 
 int main(int argc, char *argv[]) {
-  int i,j=0,*a,n,*prime;
-  
-  n=argc-1;
-  a=(int*)malloc(sizeof(int)*n);
-  prime=(int*)malloc(sizeof(int)*n);
-  for(i=0;i<n;i++)
-  {
-    a[i]=atoi(argv[i+1]);
-    if(Isprime(a[i]))
-    {
-      prime[j]=a[i];
+  int i, j = 0, *a, n, *prime;
+
+  n = argc - 1;
+  a = (int *)malloc(sizeof(int) * n);
+  prime = (int *)malloc(sizeof(int) * n);
+  for (i = 0; i < n; i++) {
+    a[i] = atoi(argv[i + 1]);
+    if (Isprime(a[i])) {
+      prime[j] = a[i];
       j++;
     }
   }
-  n=j;
-  if(n==0) {printf("No prime number\n");}
-  else{
-   display(prime,n);
-  bubbleSort(prime,n); 
-   //insertion(a,N);
+  n = j;
+  if (n == 0) {
+    printf("No prime number\n");
+  } else {
+    display(prime, n);
+    bubbleSort(prime, n);
+    // insertion(a,N);
 
-  // selectionSort(a,N);
-   display(prime,n);
+    // selectionSort(a,N);
+    display(prime, n);
     return 0;
   }
 }
-
